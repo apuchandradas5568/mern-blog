3cf8b3f0c311649e8a0040026466185c648d71b6
Apu Chandra Das <apuchandradas5568@gmail.com>
2024-03-14 15:38:36 +0530

    page routes setup

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,25 @@
-
 import "./App.css";
 import Dashboard from "./pages/Dashboard";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Projects from "./pages/Projects";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
+
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <>
-      <Dashboard />
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="sign-in" element={<SignIn />} />
+        <Route path="projects" element={<Projects />} />
+        <Route path="sign-up" element={<SignUp />} />
+        <Route path="dashboard" element={<Dashboard />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 

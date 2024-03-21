76d51f0ec1148623c177236ac92451d4789ece32
Apu Chandra Das <apuchandradas5568@gmail.com>
2024-03-22 01:09:29 +0530

    done with header component, about and projects page

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -9,43 +9,43 @@ import { toggleTheme } from "../redux/theme/themeSlice.js";
 
 export default function Header() {
   const path = useLocation().pathname;
+  const location = useLocation();
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
   const { currentUser } = useSelector((state) => state.user);
   const { theme } = useSelector((state) => state.theme);
-  const dispatch = useDispatch();
-  const [searchTerm, setSearchTerm ] = useState('')
-  const navigate = useNavigate()
+  const [searchTerm, setSearchTerm] = useState('');
 
-  const location = useLocation()
-
-  useEffect(()=>{
-    const urlParams = new URLSearchParams(location.search)
-    const searchTermFromUrl = urlParams.get('searchTerm')
-
-    if(searchTermFromUrl) {
-      setSearchTerm(searchTermFromUrl)
+  useEffect(() => {
+    const urlParams = new URLSearchParams(location.search);
+    const searchTermFromUrl = urlParams.get('searchTerm');
+    if (searchTermFromUrl) {
+      setSearchTerm(searchTermFromUrl);
     }
-
-  },[location.search]);
-
-const handleSubmit = (e) =>{
-  e.preventDefault()
-  const urlParams = new URLSearchParams(location.search)
-  urlParams.set("searchTerm", searchTerm)
-  const searchQuery = urlParams.toString()
-  navigate(`/search?${searchQuery}`)
-}
+  }, [location.search]);
 
   const handleSignout = async () => {
     try {
-      const res = await fetch("/api/user/signout", { method: "POST" });
+      const res = await fetch('/api/user/signout', {
+        method: 'POST',
+      });
       const data = await res.json();
-
-      if (res.ok) {
-        dispatch(signOutSuccess(data));
+      if (!res.ok) {
+        console.log(data.message);
+      } else {
+        dispatch(signOutSuccess());
       }
     } catch (error) {
       console.log(error.message);
     }
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const urlParams = new URLSearchParams(location.search);
+    urlParams.set('searchTerm', searchTerm);
+    const searchQuery = urlParams.toString();
+    navigate(`/search?${searchQuery}`);
   };
 
   return (
@@ -55,71 +55,67 @@ const handleSubmit = (e) =>{
           <p className="dark:text-black">MERNB</p>
         </Link>
         <form onSubmit={handleSubmit}>
-          <TextInput
-            type="text"
-            placeholder="Search..."
-            rightIcon={AiOutlineSearch}
-            className="hidden lg:inline"
-          />
-        </form>
-        <Button className="w-12 h-12 lg:hidden" color="gray" pill>
-          <AiOutlineSearch />
+        <TextInput
+          type='text'
+          placeholder='Search...'
+          rightIcon={AiOutlineSearch}
+          className='hidden lg:inline'
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
+        />
+      </form>
+      <Button className='w-12 h-10 lg:hidden' color='gray' pill>
+        <AiOutlineSearch />
+      </Button>
+      <div className='flex gap-2 md:order-2'>
+        <Button
+          className='w-12 h-10 hidden sm:inline'
+          color='gray'
+          pill
+          onClick={() => dispatch(toggleTheme())}
+        >
+          {theme === 'light' ? <FaSun /> : <FaMoon />}
         </Button>
-        <div className="flex gap-4 md:order-2">
-          <Button
-            className="w-12 h-10 hidden sm:inline"
-            color="gray"
-            pill
-            onClick={() => dispatch(toggleTheme())}
+        {currentUser ? (
+          <Dropdown
+            arrowIcon={false}
+            inline
+            label={
+              <Avatar alt='user' img={currentUser.profilePicture} rounded />
+            }
           >
-            {theme === "light" ? <FaMoon /> : <FaSun />}
-          </Button>
-          {currentUser ? (
-            <Dropdown
-              arrowIcon={false}
-              inline
-              label={
-                <Avatar alt="user" img={currentUser.profilePicture} rounded />
-              }
-            >
-              <Dropdown.Header>
-                <span className="block text-sm">@{currentUser.username}</span>
-                <span className="block text-sm font-medium truncate">
-                  {currentUser.email}
-                </span>
-              </Dropdown.Header>
-              {currentUser.isAdmin && (
-                <Link to={"/dashboard?tab=dash"}>
-                  <Dropdown.Item>Dashboard</Dropdown.Item>
-                </Link>
-              )}
-              <Link to={"/dashboard?tab=profile"}>
-                <Dropdown.Item>Profile</Dropdown.Item>
-              </Link>
-              <Dropdown.Divider />
-              <Dropdown.Item onClick={handleSignout}>Sign Out</Dropdown.Item>
-            </Dropdown>
-          ) : (
-            <Link to="/sign-in">
-              <Button className="bg-gray-300" color="gray">
-                Sign In
-              </Button>
+            <Dropdown.Header>
+              <span className='block text-sm'>@{currentUser.username}</span>
+              <span className='block text-sm font-medium truncate'>
+                {currentUser.email}
+              </span>
+            </Dropdown.Header>
+            <Link to={'/dashboard?tab=profile'}>
+              <Dropdown.Item>Profile</Dropdown.Item>
             </Link>
-          )}
-
-          <Navbar.Toggle />
-        </div>
-        <Navbar.Collapse>
-          <Navbar.Link active={path === "/"} as={"div"}>
-            <Link to="/">Home</Link>
-          </Navbar.Link>
-          <Navbar.Link active={path === "/about"} as={"div"}>
-            <Link to="/about">About</Link>
-          </Navbar.Link>
-          <Navbar.Link active={path === "/projects"} as={"div"}>
-            <Link to="/projects">Projects</Link>
-          </Navbar.Link>
-        </Navbar.Collapse>
+            <Dropdown.Divider />
+            <Dropdown.Item onClick={handleSignout}>Sign out</Dropdown.Item>
+          </Dropdown>
+        ) : (
+          <Link to='/sign-in'>
+            <Button  outline>
+              Sign In
+            </Button>
+          </Link>
+        )}
+        <Navbar.Toggle />
+      </div>
+      <Navbar.Collapse>
+        <Navbar.Link active={path === '/'} as={'div'}>
+          <Link to='/'>Home</Link>
+        </Navbar.Link>
+        <Navbar.Link active={path === '/about'} as={'div'}>
+          <Link to='/about'>About</Link>
+        </Navbar.Link>
+        <Navbar.Link active={path === '/projects'} as={'div'}>
+          <Link to='/projects'>Projects</Link>
+        </Navbar.Link>
+      </Navbar.Collapse>
       </Navbar>
     </div>
   );
--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -1,9 +1,28 @@
-import React from 'react'
-
 export default function About() {
   return (
-    <div>
-      About
+    <div className='min-h-screen flex items-center justify-center'>
+      <div className='max-w-2xl mx-auto p-3 text-center'>
+        <div>
+          <h1 className='text-3xl font font-semibold text-center my-7'>
+            About MERN' Blog
+          </h1>
+          <div className='text-md text-gray-500 flex flex-col gap-6'>
+            <p>
+              Welcome to MERN's Blog! This blog was created by MERN Ghavidel
+              as a personal project to share his thoughts and ideas with the
+              world. MERN is a passionate developer who loves to write about
+              technology, coding, and everything in between.
+            </p>
+
+            <p>
+              On this blog, you'll find weekly articles and tutorials on topics
+              such as web development, software engineering, and programming
+              languages. MERN is always learning and exploring new
+              technologies, so be sure to check back often for new content!
+            </p>
+          </div>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
